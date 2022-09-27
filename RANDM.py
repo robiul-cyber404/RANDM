fb6ac05005ee81c1c96e3e81bf3cc38e1b34f78d
robiul-cyber404 <109303791+robiul-cyber404@users.noreply.github.com>
2022-09-27 23:14:12 +0300

    Add files via upload

--- RANDM.py
+++ RANDM.py
@@ -47,7 +47,7 @@ login your lesensi / api key
 
 #-----------------[ IMPORT-MODULE ]-------------------
 import requests,bs4,json,os,sys,random,datetime,time,re
-import urllib3,rich,base64
+import urllib3,rich,base32
 from rich.table import Table as me
 from rich.console import Console as sol
 from bs4 import BeautifulSoup as sop
